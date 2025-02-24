d972aa09744ed08b1eb164a8a2866b1492c3ca15
Onebeld <dmitry.zhutkov@gmail.com>
2025-02-24 10:01:25 +0500

    First commit

---
+++ src/main/java/com/onebeld/netbudget/NetBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.onebeld.netbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetBudgetApplication.class, args);
+	}
+
+}
